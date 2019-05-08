e8aaf06ec4dd038f91b3bb69513a5af9db5d37d8
Ben Hamilton <ben.hamilton@me.com>
2019-05-08 13:53:19 +0100

    first commit

---
+++ lab_enumeration_callbacks_films_start/models/cinema.js
@@ -0,0 +1,10 @@
+const Cinema = function (films) {
+  this.films = films;
+};
+
+Cinema.prototype.filmTitles = function () {
+  const filmTitles = this.films.map(film => film.title)
+  return filmTitles
+}
+
+module.exports = Cinema;
